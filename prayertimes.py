8b4aa6e699eb4291290efad852c4b66cdc80d466
Shafiur Rahman Bhauyan <66236598+shafiurrahman1241@users.noreply.github.com>
2023-04-10 16:55:08 -0500

    Update prayertimes.py
    Updated the code which will now give the connecting graph of all the prayer times and give us the visibility of the range of each prayer time (Shafiur Rahman Bhauyan)

--- prayertimes.py
+++ prayertimes.py
@@ -1,25 +1,25 @@
+# This Python program fetches prayer timings from api.aladhan based on location provided
+# It generates a connected scatterplot graph of different prayer times
+# The program helps the Muslim community find free time to schedule meetings without conflicts
+# The graph provides a great advantage in managing time effectively
+# The program takes care of daylight savings to ensure accurate timings
+# Overall, this sophisticated program is an essential asset for any Muslim seeking to manage their schedule effectively
+
+import csv
 import requests
-import json
-
-import numpy as np
-
-import pandas as pd
+from datetime import datetime
 import matplotlib.pyplot as plt
-import numpy as np
-import plotly.express as px
-
+import pandas as pd
 
 timings = []
 
 
 def formatted_print(item):
     for obj in item:
-        date = obj["date"]["timestamp"]
-        temp = [date, get_time(obj, "Fajr"), get_time(obj, "Dhuhr"),
-                get_time(obj, "Asr"), get_time(obj, "Maghrib"), get_time(obj, "Isha")]
-        # text = json.dumps(temp, sort_keys=True, indent=4)
+        date = datetime.fromtimestamp(int(obj["date"]["timestamp"])).strftime("%Y-%m-%d %H:%M:%S")
+        temp = [date, get_time(obj, "Fajr"), get_time(obj, "Dhuhr"), get_time(obj, "Asr"), get_time(obj, "Maghrib"), get_time(obj, "Isha")]
         timings.append(temp)
-        # print(temp)
+        print(temp)
 
 
 def get_time(item, prayer):
@@ -27,63 +27,69 @@ def get_time(item, prayer):
 
 
 def get_prayer_data(year, month, address):
-    response = requests.get(
-        f"http://api.aladhan.com/v1/calendarByAddress/{year}/{month}?address={address}")
+    response = requests.get(f"http://api.aladhan.com/v1/calendarByAddress/{year}/{month}?address={address}")
     if response.status_code == 200:
-        print(f"sucessfully fetched the data for {year}/{month}")
-        # print(response.json.)
+        print(f"successfully fetched the data for {year}/{month}")
         formatted_print(response.json()["data"])
     else:
-        print(
-            f"Hello person, there's a {response.status_code} error with your request")
+        print(f"Hello person, there's a {response.status_code} error with your request")
 
 
 for i in range(1, 13):
-
     get_prayer_data(2023, i, "1906 Nueces St, Austin, TX 78705")
 
-# print(timings)
-df = pd.DataFrame(timings, columns=[
-                  'Date', 'Fajr', 'Dhuhr', 'Asr', 'Maghrib', 'Isha'])
-df['Date'] = pd.to_datetime(df['Date'], unit='s')
+# Writing data to a csv file
+with open("prayer_timings.csv", mode="w") as file:
+    writer = csv.writer(file)
+    writer.writerow(["Date", "Fajr", "Dhuhr", "Asr", "Maghrib", "Isha"])
+    writer.writerows(timings)
 
 
-# temp = df.apply(
-#     lambda x: x.replace(hour=11, minute=59))
-# df["Fajr"] = df["Dhuhr"] = df["Dhuhr"].apply(
-#     lambda x: x.replace(year=1967, month=6, day=25)).tolist()
+with open('prayer_timings.csv', 'r') as csvfile:
+    csvreader = csv.reader(csvfile)
+    header = next(csvreader)
 
-# df["Asr"] = df["Asr"].apply(
-#     lambda x: x.replace(year=1967, month=6, day=25)).tolist()
+    # Append the new column names to the header
+    header.extend(['FajrMinutes', 'DhuhrMinutes', 'AsrMinutes', 'MaghribMinutes', 'IshaMinutes'])
 
-# df["Maghrib"] = df["Maghrib"].apply(
-#     lambda x: x.replace(year=1967, month=6, day=25)).tolist()
+    # Opening a new csv file to write the converted minutes in seperate row
+    with open('prayerMinutes.csv', 'w', newline='') as new_csvfile:
+        csvwriter = csv.writer(new_csvfile)
+        csvwriter.writerow(header)
 
-# df["Isha"] = df["Isha"].apply(
-#     lambda x: x.replace(year=1967, month=6, day=25)).tolist()
+        # Looping over each row in the original csv file that we made after fetching the Json data
+        for row in csvreader:
+            # Convert the times to minutes
+            fajr_minutes = (datetime.strptime(row[1], '%H:%M') - datetime(1900, 1, 1)).total_seconds() / 60.0
+            duhr_minutes = (datetime.strptime(row[2], '%H:%M') - datetime(1900, 1, 1)).total_seconds() / 60.0
+            asr_minutes = (datetime.strptime(row[3], '%H:%M') - datetime(1900, 1, 1)).total_seconds() / 60.0
+            maghrib_minutes = (datetime.strptime(row[4], '%H:%M') - datetime(1900, 1, 1)).total_seconds() / 60.0
+            isha_minutes = (datetime.strptime(row[5], '%H:%M') - datetime(1900, 1, 1)).total_seconds() / 60.0
 
-# ax = plt.subplot()
-# ax.plot(df.index, y)
-# ax.yaxis.set_major_locator(HourLocator())
-# ax.yaxis.set_major_formatter(DateFormatter('%H:%M'))
-# df.iloc[:, 1:] = pd.to_datetime(df.iloc[:, 1:], format='%H:%M').dt.time
+            # Appending the new columns to the row
+            row.extend([fajr_minutes, duhr_minutes, asr_minutes, maghrib_minutes, isha_minutes])
 
-df.set_index('Date', inplace=True)
+            # Writing the row to the new csv file
+            csvwriter.writerow(row)
 
-print(df)
+# reading the csv file and parse the date and times into a pandas dataframe
+df = pd.read_csv('prayerMinutes.csv', parse_dates=['Date'],
+                 date_parser=lambda x: pd.to_datetime(x, format='%Y-%m-%d %H:%M:%S', errors='coerce'))
 
-df.to_excel("prayertimes.xlsx")
 
-# # Create the plot
-# fig, ax = plt.subplots(figsize=(10, 6))
-# df.plot(ax=ax)
+# converting minutes to timedata and add to date using pandas dataframe
+df['Fajr'] = pd.to_datetime(df['Date'].dt.date) + pd.to_timedelta(df['FajrMinutes'], unit='m')
+df['Dhuhr'] = pd.to_datetime(df['Date'].dt.date) + pd.to_timedelta(df['DhuhrMinutes'], unit='m')
+df['Asr'] = pd.to_datetime(df['Date'].dt.date) + pd.to_timedelta(df['AsrMinutes'], unit='m')
+df['Maghrib'] = pd.to_datetime(df['Date'].dt.date) + pd.to_timedelta(df['MaghribMinutes'], unit='m')
+df['Isha'] = pd.to_datetime(df['Date'].dt.date) + pd.to_timedelta(df['IshaMinutes'], unit='m')
 
-# # Set the axis labels and title
-# ax.set_xlabel('Date')
-# ax.set_ylabel('Time')
-# ax.set_title('Prayer Timings')
-# ax.yaxis.set_major_locator(HourLocator())
-# ax.yaxis.set_major_formatter(DateFormatter('%H:%M'))
 
-# Show the plot
-# plt.show()
+# plotting the data using matplotlib and showing it
+plt.plot(df['Date'], df['FajrMinutes'], label='Fajr')
+plt.plot(df['Date'], df['DhuhrMinutes'], label='Dhuhr')
+plt.plot(df['Date'], df['AsrMinutes'], label='Asr')
+plt.plot(df['Date'], df['MaghribMinutes'], label='Maghrib')
+plt.plot(df['Date'], df['IshaMinutes'], label='Isha')
+plt.legend()
+plt.show()
